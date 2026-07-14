87303c02fc004ac52cf273adc6f5c96d4e91c0da
jackson felipe <jacksonfelipeead@gmail.com>
2026-07-14 20:02:55 -0300

    Fix Resend API key error and configure dotenv path

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const path = require("path");
-require("dotenv").config({ path: path.join(__dirname, ".env") });
+const dotenvPath = path.resolve(__dirname, ".env");
+require("dotenv").config({ path: dotenvPath });
 
 const express = require("express");
 const cors = require("cors");
